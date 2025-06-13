d26356809d11cd4d0f7c08dc7ec008d8f77326cd
maxime_carlier <28m06c02@gmail.com>
2025-06-13 14:26:15 +0200

    change coyo dataset for support batch

--- vlac/dataset/coyo.py
+++ vlac/dataset/coyo.py
@@ -5,11 +5,13 @@ import webdataset as wds
 from torch.utils.data import IterableDataset
 
 from vlac.dataset.config import COYO_PATH, COYO_LENGTH, COYO_SHUFFLE
+from vlac.constants import DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 
 
 class COYOWebDatasetIterable(IterableDataset):
-    def __init__(self, img_preprocess, tokenizer, tars_path_pattern=os.path.join(COYO_PATH, "tars/*.tar"), length: int = COYO_LENGTH, shuffle: int = COYO_SHUFFLE):
+    def __init__(self, img_preprocess, tokenizer, tars_path_pattern=os.path.join(COYO_PATH, "tars/*.tar"), length: int = COYO_LENGTH, shuffle: int = COYO_SHUFFLE, batch_size: int = 1):
         self.length = length
+        self.batch_size = batch_size
         self.img_preprocess = img_preprocess
         self.tokenizer = tokenizer
         self.dataset = wds.DataPipeline(
@@ -19,21 +21,27 @@ class COYOWebDatasetIterable(IterableDataset):
             wds.shuffle(shuffle),
             wds.decode("pil"),
             wds.to_tuple("png", "text"),
+            wds.batched(batch_size),
             wds.map(self.preprocess),
         ).with_length(self.length)
 
+    @staticmethod
+    def __add_im_tokens(txt):
+        return f'{DEFAULT_IM_START_TOKEN}{DEFAULT_IM_END_TOKEN} : {txt}'
+
     def preprocess(self, x):
         img, txt = x
-        img = self.img_preprocess(img, return_tensors="pt")["pixel_values"][0]
-        text_tokens = self.tokenizer(txt, return_tensors="pt")["input_ids"][0]
+        img = self.img_preprocess(img, return_tensors="pt")["pixel_values"]
+        txt = [self.__add_im_tokens(t) for t in txt] if isinstance(txt, list) else self.__add_im_tokens(txt)
+        text_tokens = self.tokenizer(txt, return_tensors="pt", padding=True)
         return img, text_tokens
 
     def __iter__(self):
         for img, txt_tokens in self.dataset:
             yield {
-                "vision": img.contiguous(),
-                "text_tokens": txt_tokens.contiguous()
+                "vision": img,
+                "text_tokens": txt_tokens
             }
 
     def __len__(self):
-        return self.length
+        return self.length // self.batch_size
