cda21973f10e9dc1adc338d3d355ea1a352ce310
maxime_carlier <28m06c02@gmail.com>
2025-06-17 09:53:17 +0200

    add func for load part of weights

---
+++ vlac/model/utils.py
@@ -0,0 +1,29 @@
+import json
+from typing import List
+
+from safetensors.torch import safe_open
+
+from vlac.vlac import *
+
+
+def load_weights_of_keys_start_with(path: str, start: str | List[str]):
+    index = json.load(open(os.path.join(path, "model.safetensors.index.json")))["weight_map"]
+    files = {}
+    states = {k: {} for k in start} if isinstance(start, List) else {start: {}}
+    for k, v in index.items():
+        local_start = None
+        for s in start:
+            if not k.startswith(s):
+                continue
+            local_start = s
+            break
+        if local_start is None:
+            continue
+        if v not in files:
+            files[v] = safe_open(os.path.join(path, v), framework="pt", device="cpu")
+        f = files[v]
+        key = k[len(local_start):]
+        if key.startswith("."):
+            key = key[1:]
+        states[local_start][key] = f.get_tensor(k)
+    return states
