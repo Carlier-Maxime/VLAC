01932b97ea5d2a655418319d5e268d35b46c6713
maxime_carlier <28m06c02@gmail.com>
2025-06-24 12:57:25 +0200

    add remove_multiprocess_args and tweak check ntasks

--- vlac/utils/args.py
+++ vlac/utils/args.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser, Namespace
+from copy import deepcopy
 
 
 def add_multiprocess_args(parser: ArgumentParser, max_ntasks: int = None) -> ArgumentParser:
@@ -9,11 +10,22 @@ def add_multiprocess_args(parser: ArgumentParser, max_ntasks: int = None) -> Arg
 
 def check_multiprocess_args(parser: ArgumentParser, args: Namespace, max_ntasks: int = None) -> Namespace:
     if args.ntasks is None:
-        args.ntasks = max_ntasks
-    if args.ntasks < 1 or args.ntasks > max_ntasks:
+        args.ntasks = 1 if max_ntasks is None else max_ntasks
+    if args.ntasks < 1 or (max_ntasks is not None and args.ntasks > max_ntasks):
         parser.error(f"the ntasks must be between 1 and {max_ntasks}")
     if args.procid is not None and (args.procid < 0 or args.procid >= args.ntasks):
         parser.error("the procid must be between 0 and ntasks-1")
     args.master = args.procid is None or args.procid == 0
     print(f"task {args.procid} of {args.ntasks}")
     return args
+
+
+def remove_multiprocess_args(args: Namespace) -> Namespace:
+    new_args = deepcopy(args)
+    try:
+        delattr(new_args, 'procid')
+        delattr(new_args, 'ntasks')
+        delattr(new_args, 'master')
+    except AttributeError:
+        pass
+    return new_args
