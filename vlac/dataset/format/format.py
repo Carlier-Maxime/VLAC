8c271cf2f781dfb0a7135558a6a9546e8719f6f6
maxime_carlier <28m06c02@gmail.com>
2025-06-26 15:23:37 +0200

    prepare resume support for format

--- vlac/dataset/format/format.py
+++ vlac/dataset/format/format.py
@@ -1,4 +1,5 @@
 import argparse
+import glob
 import json
 import os
 from abc import abstractmethod, ABC
@@ -31,17 +32,66 @@ class FormatDataset(ABC):
     def format_from_args_(cls, args: argparse.Namespace, **kwargs):
         return cls(**kwargs).format(args.dataset, args.output, args.parquet_size)
 
+    @staticmethod
+    def assert_info(info_path: str, parquet_size: int, parquets: List[str]):
+        info = json.load(open(info_path))
+        assert info["parquet_count"] == len(parquets), f'the number of parquet files ({len(parquets)}) mismatch with info.json ({info["parquet_count"]}).'
+        assert info["average_memory_per_parquet"] == parquet_size, f'the average memory occupied per parquet ({info["average_memory_per_parquet"]}) mismatch with parquet_size given ({parquet_size}).'
+        assert info["total_samples"] == sum(info["samples_per_parquet"]), f'the total number of samples ({info["total_samples"]}) mismatch with the samples per parquet {info["samples_per_parquet"]}.'
+        assert info["max_indices_per_parquet"] == (np.array(info["samples_per_parquet"]).cumsum() - 1).tolist(), f'the maximum indices per parquet {info["max_indices_per_parquet"]} mismatch with samples per parquet {info["samples_per_parquet"]}.'
+
+    @staticmethod
+    def complete_missing_info(info: dict, parquets_missing: List[str]) -> dict:
+        for parquet in tqdm(parquets_missing, desc='complete missing info', unit='parquet'):
+            df = pd.read_parquet(parquet)
+            info["samples_per_parquet"].append(df.shape[0])
+            info["max_indices_per_parquet"].append(info["max_indices_per_parquet"][-1] + df.shape[0])
+            info["total_samples"] += df.shape[0]
+            info["parquet_count"] += 1
+        return info
+
+    def resume(self, resume_path: str, parquet_size: int) -> int:
+        assert os.path.isdir(resume_path), 'path must be a directory if it already exists.'
+        nb_files = len(os.listdir(resume_path))
+        if nb_files > 0:
+            print(f'The directory already exists and not empty. The format is resumed.')
+            parquets = sorted(glob.glob(os.path.join(resume_path, "*.parquet")))
+            info_path = os.path.join(resume_path, "info.json")
+            try:
+                self.assert_info(info_path, parquet_size, parquets)
+            except OSError or AssertionError as e:
+                print("info.json is invalid, info backup is used to resume the format.")
+                info_backup_path = os.path.join(resume_path, "info_backup.json")
+                if not os.path.exists(info_backup_path): raise RuntimeError("info backup not found, and original info is invalid !", e)
+                info_backup = json.load(open(info_backup_path))
+                if info_backup["parquet_count"] == len(parquets):
+                    os.rename(info_backup_path, info_path)
+                elif info_backup["parquet_count"] < len(parquets):
+                    info_backup = self.complete_missing_info(info_backup, parquets[info_backup["parquet_count"]:])
+                    json.dump(info_backup, open(info_path, "w"), indent=None, separators=(',', ':'))
+                    os.remove(info_backup_path)
+                else:
+                    raise RuntimeError(f"Resume cannot be done, info invalid and the info backup cannot be used because the number of parquet files is less than the number of parquet files in the info_backup.json file.", e)
+                self.assert_info(info_path, parquet_size, parquets)
+            return json.load(open(info_path))["total_samples"]
+        return 0
+
     def format(self, input_path: str, output_path: str, parquet_size: int):
         assert os.path.abspath(input_path) != os.path.abspath(output_path), 'the input and output path must be not the same.'
-        os.makedirs(output_path, exist_ok=True)
+        resume_samples = 0
+        if os.path.exists(output_path):
+            resume_samples = self.resume(output_path, parquet_size)
+        else: os.makedirs(output_path)
 
         part_df_mem = 0
         part_df: List[pd.DataFrame] = []
         sizes = []
         i = 1
         save_paths = []
+        iterator = self.get_iterator(input_path, parquet_size)
+        if resume_samples > 0: iterator = self.resume_to_samples(iterator, resume_samples)
         step_data = self.init_step_data(input_path, parquet_size)
-        for data in self.get_iterator(input_path, parquet_size):
+        for data in iterator:
             df = self.make_df(data, step_data)
             if df is None: continue
             part_df.append(df)
@@ -50,6 +100,7 @@ class FormatDataset(ABC):
                 df = part_df[0] if len(part_df) == 1 else pd.concat(part_df)
                 residual_df, saved_df_len = FormatDataset.save_parquet(df, FormatDataset.out_path(output_path, i, save_paths), parquet_size)
                 sizes.append(saved_df_len)
+                self.save_info(output_path, sizes, parquet_size)
                 part_df_mem = residual_df.memory_usage(deep=True).sum()
                 part_df = [residual_df]
                 i += 1
@@ -69,13 +120,23 @@ class FormatDataset(ABC):
             splitext = os.path.splitext(os.path.basename(path))
             os.rename(path, os.path.join(output_path, f"{int(splitext[0]):0{digits}d}-of-{i:0{digits}d}{splitext[1]}"))
 
+        self.save_info(output_path, sizes, parquet_size)
+
+    @staticmethod
+    def save_info(output_dir: str, sizes: List[int], parquet_size: int):
+        out_path = os.path.join(output_dir, "info.json")
+        backup_path = os.path.join(output_dir, "info_backup.json")
+        if os.path.exists(out_path):
+            os.rename(out_path, backup_path)
         json.dump({
             "parquet_count": len(sizes),
             "average_memory_per_parquet": parquet_size,
             "samples_per_parquet": sizes,
             "max_indices_per_parquet": (np.array(sizes).cumsum() - 1).tolist(),
             "total_samples": sum(sizes)
-        }, open(os.path.join(output_path, "info.json"), "w"), indent=None, separators=(',', ':'))
+        }, open(out_path, "w"), indent=None, separators=(',', ':'))
+        if os.path.exists(backup_path):
+            os.remove(backup_path)
 
     @staticmethod
     def out_path(output: str, i: int, save_paths: List[str] = None):
@@ -109,3 +170,7 @@ class FormatDataset(ABC):
     @abstractmethod
     def make_last_df(self, step_data: argparse.Namespace) -> pd.DataFrame | None:
         pass
+
+    @abstractmethod
+    def resume_to_samples(self, iterator: Iterable, resume_samples: int) -> Iterable:
+        raise NotImplementedError
