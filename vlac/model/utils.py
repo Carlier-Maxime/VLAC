ed15258aa5d2b0cd05087aad202bcea6786c52f8
maxime_carlier <28m06c02@gmail.com>
2025-06-27 11:54:44 +0200

    add method for replace weights

--- vlac/model/utils.py
+++ vlac/model/utils.py
@@ -1,15 +1,20 @@
 import glob
 import json
 import os
+import shutil
 from typing import List
 
-from safetensors.torch import safe_open
+from safetensors.torch import safe_open, save
 
 
-def load_weights_of_keys_startswith(path: str, start: str | List[str]) -> dict:
-    start_islist = isinstance(start, List)
-    states = {k: {} for k in start} if start_islist else {start: {}}
-    index_path = os.path.join(path, "model.safetensors.index.json")
+def __get_index_path(path: str) -> str:
+    return os.path.join(path, "model.safetensors.index.json")
+
+
+def load_weights(path: str, start: str | List[str] = None) -> dict:
+    start_islist = False if start is None else isinstance(start, list)
+    states = {k: {} for k in start} if start_islist else {} if start is None else {start: {}}
+    index_path = __get_index_path(path)
 
     def get_local_start(key: str) -> str | None:
         if start_islist:
@@ -18,13 +23,19 @@ def load_weights_of_keys_startswith(path: str, start: str | List[str]) -> dict:
                     continue
                 return s
             return None
+        elif start is None:
+            return key
         return start if key.startswith(start) else None
 
     def add_tensor(f: safe_open, full_key: str, local_start: str):
-        key = full_key[len(local_start):]
+        same = local_start == full_key
+        key = full_key[0 if same else len(local_start):]
         if key.startswith("."):
             key = key[1:]
-        states[local_start][key] = f.get_tensor(k)
+        if same:
+            states[full_key] = f.get_tensor(k)
+        else:
+            states[local_start][key] = f.get_tensor(k)
 
     if os.path.exists(index_path):
         index = json.load(open(index_path))["weight_map"]
@@ -48,3 +59,46 @@ def load_weights_of_keys_startswith(path: str, start: str | List[str]) -> dict:
             if local_start is None: continue
             add_tensor(f, k, local_start)
     return states
+
+
+def weights_by_file(weights: dict, index_path: str) -> dict:
+    index = json.load(open(index_path))["weight_map"]
+    new_weights = {}
+    for k, v in index.items():
+        if v not in new_weights:
+            new_weights[v] = {}
+        new_weights[v][k] = weights[k]
+    return new_weights
+
+
+def replace_weights_in_safetensors(base_path: str, replacement_path: str, key_prefix: str = "", output_path: str = None):
+    weights = load_weights(base_path)
+    replace_weights = load_weights(replacement_path)
+    if key_prefix[-1] != '.': key_prefix += '.'
+    for k in replace_weights.keys():
+        key = key_prefix + k
+        if key in weights:
+            weights[key] = replace_weights[k]
+        else:
+            print(f"key '{key}' not found in base model.")
+
+    if output_path is None:
+        output_path = base_path
+    index_path = __get_index_path(base_path)
+
+    if os.path.exists(index_path):
+        assert not output_path.endswith(".safetensors"), "output path must be a directory if base path contains multiple safetensors files."
+        os.makedirs(output_path, exist_ok=True)
+        if output_path != base_path: shutil.copy(index_path, __get_index_path(output_path))
+        weights = weights_by_file(weights, index_path)
+        for file_path, weights_file in weights.items():
+            with open(os.path.join(output_path, file_path), "wb") as f:
+                f.write(save(weights_file))
+    else:
+        if not output_path.endswith(".safetensors"):
+            os.makedirs(output_path, exist_ok=True)
+            save_path = os.path.join(output_path, "model.safetensors")
+        else:
+            save_path = output_path
+        with open(save_path, 'wb') as f:
+            f.write(save(weights))
